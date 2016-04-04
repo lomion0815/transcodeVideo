d446be8d8ad634f807995d9c9781f1e7437e0b7e
lomion0815 <lomion@aon.at>
2016-04-04 09:20:11 +0200

    Change control to xml-files Added concat functionality

--- transcodeVideo.py
+++ transcodeVideo.py
@@ -18,9 +18,11 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
 import subprocess
-#import os
+import os
+import sys
 import argparse
 import re
+import xml.etree.ElementTree as ET
 
 #-------------------------------------------------------------------------------
 # CONFIGURABLE SETTINGS
@@ -33,13 +35,16 @@ CRF = '21'
 PROFILE = 'baseline'
 
 # encoding speed:compression ratio
-PRESET = 'medium'
+PRESET = 'slow'
 
 # path to ffmpeg bin
 FFMPEG_PATH = 'D:\\tmp\\ffmpeg.exe'
 
 # path to ffprobe bin
 FFPROBE_PATH = 'D:\\tmp\\ffprobe.exe'
+
+# path to temp directory
+TEMPDIR = 'tmp'
 
 #-------------------------------------------------------------------------------
 # encoding script
@@ -48,14 +53,18 @@ FFPROBE_PATH = 'D:\\tmp\\ffprobe.exe'
 def encode(input,output,start=None,duration=None,volume=None,resolution=None):
     try:
         command = [FFMPEG_PATH]
+
+        # Input file
+        command += [ '-i', input]
+        
         if start is not None:
-            command += [ '-ss', start]
-        
-        command += [ '-i', input,
-            '-y', '-c:v', 'libx264', '-preset', PRESET, '-profile:v', PROFILE, '-crf', CRF,]
-        
+            command += [ '-ss', str(start)]
+            
         if duration is not None:
-            command += [ '-t', duration]
+            command += [ '-t', str(duration)]
+        
+        # Video codec
+        command += [ '-y', '-c:v', 'libx264', '-preset', PRESET, '-profile:v', PROFILE, '-crf', CRF]
         
         # Create filter string
         filters = ['yadif']
@@ -72,19 +81,52 @@ def encode(input,output,start=None,duration=None,volume=None,resolution=None):
 #    finally:
         # always cleanup even if there are errors
 
-
+def encodeCopyConcat(output):
+    try:
+        command = [FFMPEG_PATH]
+        command += [ '-f' , 'concat' , '-i', os.path.join(TEMPDIR,"concat.txt"),
+            '-y', '-c:v', 'copy', '-c:a', 'copy', '-map',  '0' , '-sn']
+        command += [output]
+        print(command)
+        subprocess.call(command)                # encode the video!
+    except:
+        print("Error encoding:",input)
+        
 if __name__ == "__main__":
     # Parsing arguments
     parser = argparse.ArgumentParser(description='Encode a video for Samsung Galaxy S5 Mini.')
-    parser.add_argument('input', help='input video file or textfile (.txt) containing a list of files')
+    parser.add_argument('input', help='xml-file describing input file')
     parser.add_argument('output', help='output video file')
-    parser.add_argument('-ss','--start', type=float, help='start time in seconds')
-    parser.add_argument('-t','--duration', type=float, help='duration in seconds')
     args = parser.parse_args()
-    input = args.input
+    
+    try:
+        xml = ET.ElementTree(file=args.input)
+    except FileNotFoundError:
+        print ("File",args.input,"not found")
+        sys.exit(1)
+    #except:
+        #print ("Error opening file File",args.input)
+        #sys.exit(1)       
+            
+    
+    path = xml.find('path').text
+    basename = xml.find('basename').text
+    uncutlist_xml = xml.find('uncutlist')
+    startstoplist = list(uncutlist_xml.iter())
+    uncutlist = []
+    start = startstoplist[1].text
+    stop = None
+    for x in range(1,len(startstoplist),2):
+        if x+1 < len(startstoplist):
+            uncutlist.append([startstoplist[x].text,startstoplist[x+1].text])
+            stop = startstoplist[x+1].text
+        else:
+            uncutlist.append([startstoplist[x].text,None])
+            stop = None        
+
+    input = os.path.join(path,basename)
     output = args.output
-    start = args.start
-    duration = args.duration
+    print(input, output)
 
     # Probing input file
     command= [FFMPEG_PATH, '-i', input, '-vn', '-sn', '-af', 'volumedetect', '-f', 'null', '/dev/null']
@@ -105,7 +147,22 @@ if __name__ == "__main__":
         resolution=[int(x) for x in groups]
     except:
         resolution=None
-
-    encode(input,output,resolution=resolution)
+    if len(uncutlist) == 0:
+        encode(input,output,resolution=resolution)
+    else:
+        concat_file = open(os.path.join(TEMPDIR,"concat.txt"), "w", encoding="utf-8")
+        for x in range(len(uncutlist)):
+            tempname = "temp_"+str(x)+".mkv"
+            tempname = os.path.join(TEMPDIR, tempname)
+            concat_string="file '"+tempname+"'\n"
+            concat_file.write(concat_string)
+            if uncutlist[x][1] == None:
+                length = None
+            else:
+                length = int(uncutlist[x][1]) - int(uncutlist[x][0])
+            encode(input,tempname,resolution=resolution,start=uncutlist[x][0],duration=length)
+        concat_file.close()
+        encodeCopyConcat(output)
+        
     
 
